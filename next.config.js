2e8caf790958d34d85184b4f2ab1b7805f4374a1
Christian Gonçalves <christiangoncalvesr@gmail.com>
2022-02-06 15:02:13 -0300

    feat: add and configure next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
